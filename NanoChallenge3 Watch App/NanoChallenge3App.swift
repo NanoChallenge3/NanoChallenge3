88ee5f74c40404f3e63bb762b922d1e91e2ff2b3
albert <albert.silva@binus.ac.id>
2023-07-13 17:42:14 +0700

    Initial Commit

---
+++ NanoChallenge3 Watch App/NanoChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge3App.swift
+//  NanoChallenge3 Watch App
+//
+//  Created by Albert Silva on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NanoChallenge3/NanoChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge3App.swift
+//  NanoChallenge3
+//
+//  Created by Albert Silva on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
